978af81e977083f5678cd6ec6055311a4494385c
Gooozeee <Michalguzym@gmail.com>
2023-10-28 15:56:06 +0100

    Feat: Setting up initial entities
    1. Created Database Column and table entities
    2. Created the enum for valid entity types and extensions for descriptions

---
+++ DynamicSqlToGraphQl.Domain/ValueObjects/SqlDataTypes.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace DynamicSqlToGraphQl.Domain.ValueObjects
+{
+    // Enumeration which holds all valid SQL Server Data types
+    public enum SqlDataTypes
+    {
+        bigint,
+        numeric,
+        bit,
+        smallint,
+        [Description("decimal")]
+        sqldecimal,
+        smallmoney,
+        [Description("int")]
+        sqlint,
+        tinyint,
+        money,
+        [Description("float")]
+        sqlfloat,
+        real,
+        date,
+        datetimeoffset,
+        datetime2,
+        smalldatetime,
+        datetime,
+        time,
+        [Description("char")]
+        sqlchar,
+        varchar,
+        text,
+        nchar,
+        nvarchar,
+        ntext,
+        cursor,
+        rowversion,
+        hierarchyid,
+        uniqueidentifier,
+        [Description("sql_variant")]
+        sqlvariant,
+        xml,
+        table
+    }
+}
